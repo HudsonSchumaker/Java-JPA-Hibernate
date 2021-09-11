552a2c90586d12509ab804986be46bb0156e7ea7
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-09-11 21:55:50 +0200

    Update MovimentacaoDao.java

--- Java8/src/br/com/schumaker/jpa/modelo/dao/MovimentacaoDao.java
+++ Java8/src/br/com/schumaker/jpa/modelo/dao/MovimentacaoDao.java
@@ -7,6 +7,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
+ 
+ /**
+ * @author Hudson Schumaker
+ */
 public class MovimentacaoDao {
 
     private final EntityManager em;
